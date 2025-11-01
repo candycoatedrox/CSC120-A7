eb7ad686fcafe21a351be0a89f3f097c944298d2
candycoatedrox <60051903+candycoatedrox@users.noreply.github.com>
2025-11-01 00:52:51 -0400

    Implement Library

--- House.java
+++ House.java
@@ -9,6 +9,8 @@ public class House extends Building implements HouseRequirements {
         super(name, address, nFloors);
         this.residents = new ArrayList<>();
         this.hasDiningRoom = hasDiningRoom;
+
+        // The little print statements in the constructors are so cute, I can't bear to get rid of them
         System.out.println("You have built a house: 🏠");
     }
 
@@ -31,6 +33,25 @@ public class House extends Building implements HouseRequirements {
 
     public boolean isResident(Student s) {
         return this.residents.contains(s);
+    }
+
+    public static void main(String[] args) {
+        Student piper = new Student("Piper Wurman", "9912345", 2029);
+        Student scout = new Student("Scout Bookham", "9987765", 2029);
+        House myHouse = new House("Ziskind House", "1 Chapin Way", 3, true);
+
+        System.out.println(myHouse.getName() + ", " + myHouse.getAddress() + ", " + myHouse.getFloors() + " floors, hasDiningRoom = " + myHouse.hasDiningRoom());
+        System.out.println(myHouse.nResidents() + " residents");
+
+        System.out.println(myHouse.isResident(piper));
+        myHouse.moveIn(piper);
+        myHouse.moveIn(scout);
+        System.out.println(myHouse.isResident(piper));
+        System.out.println(myHouse.nResidents() + " residents");
+        if (myHouse.moveOut(piper) == piper) {
+            System.out.println("Piper moved out");
+        }
+        System.out.println(myHouse.isResident(piper));
     }
 
 }
--- Library.java
+++ Library.java
@@ -1,12 +1,93 @@
-/* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
+import java.util.Set;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+public class Library extends Building implements LibraryRequirements {
+
+    private Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+        super(name, address, nFloors);
+        this.collection = new Hashtable<>();
+
+        System.out.println("You have built a library: 📖");
     }
-  
+
+    public void addTitle(String title) {
+        if (this.containsTitle(title)) {
+            throw new RuntimeException("Title already in collection");
+        }
+
+        this.collection.put(title, true);
+    }
+
+    public String removeTitle(String title) {
+        if (!this.containsTitle(title)) {
+            throw new RuntimeException("Title not in collection");
+        }
+
+        this.collection.remove(title);
+        return title;
+    }
+
+    public void checkOut(String title) {
+        if (!this.isAvailable(title)) {
+            throw new RuntimeException("Title already checked out");
+        }
+
+        this.collection.put(title, false);
+    }
+
+    public void returnBook(String title) {
+        if (this.isAvailable(title)) {
+            throw new RuntimeException("Title is not checked out");
+        }
+
+        this.collection.put(title, true);
+    }
+
+    public boolean containsTitle(String title) {
+        return this.collection.containsKey(title);
+    }
+
+    public boolean isAvailable(String title) {
+        if (!this.containsTitle(title)) {
+            throw new RuntimeException("Title does not exist in collection");
+        }
+
+        return this.collection.get(title);
+    }
+
+    public void printCollection() {
+        int n = 1;
+        Set<String> titles = this.collection.keySet();
+
+        for (String title : titles) {
+            if (this.isAvailable(title)) {
+                System.out.println(n + ". " + title + " (available)");
+            } else {
+                System.out.println(n + ". " + title + " (checked out)");
+            }
+
+            n += 1;
+        }
+    }
+
     public static void main(String[] args) {
-      new Library();
+        Library myLibrary = new Library("Neilson Library", "1 Chapin Way", 4);
+        myLibrary.addTitle("The Great Gatsby");
+        myLibrary.addTitle("Calling In");
+        myLibrary.addTitle("The House in the Cerulean Sea");
+        myLibrary.printCollection();
+
+        myLibrary.checkOut("The Great Gatsby");
+        System.out.println("The Great Gatsby is available: " + myLibrary.isAvailable("The Great Gatsby"));
+        System.out.println("The Great Gatsby is in the library: " + myLibrary.containsTitle("The Great Gatsby"));
+        System.out.println("Gideon the Ninth is in the library: " + myLibrary.containsTitle("Gideon the Ninth"));
+        myLibrary.printCollection();
+        myLibrary.returnBook("The Great Gatsby");
+
+        myLibrary.removeTitle("The Great Gatsby");
+        myLibrary.printCollection();
     }
-  
-  }
+
+}
